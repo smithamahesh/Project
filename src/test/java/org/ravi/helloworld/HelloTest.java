2a031b6d41930f8a246dab8232546396b67b9248
smithamahesh <36659746+smithamahesh@users.noreply.github.com>
2018-02-21 12:12:59 +0530

    Update HelloTest.java

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -21,7 +21,7 @@ public class HelloTest {
 	public void beforeTest() {
 		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe")
 		driver = new FirefoxDriver();*/
-		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe")
+		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/Helloworld.war/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
